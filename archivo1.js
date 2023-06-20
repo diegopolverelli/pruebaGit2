42c9a5d257174c30795132cb6cf9f26f185055a9
Diego Polverelli <diegopolverelli@hotmail.com>
2023-06-19 23:50:44 -0300

    cambio 4

--- archivo1.js
+++ archivo1.js
@@ -4,14 +4,5 @@ console.log(nombre)
 let apellido="Polverelli"
 console.log(apellido)
 
-nombre="Romina"
-console.log(nombre)
-
-apellido="Mandel"
-console.log(apellido)
-
-nombre="Paula"
-console.log(nombre)
-
 apellido="Martinez"
 console.log(apellido)
